6b8f3076df58e3abba853f8a538e141081c437e2
Zawent <100706312+Zawent@users.noreply.github.com>
2023-10-19 11:15:22 -0500

    Decoracion de apartado fichas

--- ficha/create.php
+++ ficha/create.php
@@ -1,32 +1,65 @@
+<?php
+session_start();
+
+$id_user = $_SESSION['id_user'];
+
+if (!$id_user) {
+    header('Location: index.php');
+    return;
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-16">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crear Ficha</title>
+    <link rel="stylesheet" href="styleCreate.css">
+    <link rel="stylesheet" href="../css/comun.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
+<?php
+    // At top:
+    require('../comun/header.php'); 
+    ?>
+
+    <?php
+    // At top:
+    require('../comun/navbar.php'); 
+    ?>
+    <div class= "contenedor">
     <h1>Crear Ficha</h1>
     <form method="post" action="save.php">
         <input type="hidden" name="id">
 
-        <div class="input-group mb-3">
+        <div class="inputhola">
             <label for="numero">Número:</label>
             <input id="numero" name="numero" type="text">
         </div>
 
-        <div class="input-group mb-3">
+        <div class="inputhola">
             <label for="programa">Programa:</label>
             <input id="programa" name="programa" type="text">
         </div>
 
-        <div class="input-group mb-3">
+        <div class="inputhola">
             <label for="lider">Líder:</label>
             <input id="lider" name="lider" type="text">
         </div>
 
         <button type="submit" name="guardar">Guardar</button>
-        <a class="A" href="index.php"><button>Volver a la página de inicio</button></a>
+        
     </form>
+    <a class="A" href="index.php"><button>Volver a la página de inicio</button></a>
+    </div>
 </body>
 </html>
--- ficha/edit.php
+++ ficha/edit.php
@@ -1,4 +1,18 @@
 <?php
+session_start();
+
+$id_user = $_SESSION['id_user'];
+
+if (!$id_user) {
+    header('Location: index.php');
+    return;
+}
+
+?>
+
+
+
+<?php
         if (isset($_GET['id'])) {
             $id = $_GET['id'];
 
@@ -31,30 +45,46 @@
     <meta charset="UTF-16">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Ficha</title>
+    <link rel="stylesheet" href="styleEdit.css">
+    <link rel="stylesheet" href="../css/comun.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
-<body>
+
+<body>  
+<?php
+    // At top:
+    require('../comun/header.php'); 
+    ?>
+
+    <?php
+    // At top:
+    require('../comun/navbar.php'); 
+    ?>
+    <div class= "contenedor">
     <h1>Editar Ficha</h1>
     <form method="post" action="update.php">
         <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
 
-        <div class="input-group mb-3">
+        <div class="inputhola">
             <label for="numero">Número:</label>
             <input id="numero" name="numero" type="text" value="<?php echo $row['numero']; ?>">
         </div>
 
-        <div class="input-group mb-3">
+        <div class="inputhola">
             <label for="programa">Programa:</label>
             <input id="programa" name="programa" type="text" value="<?php echo $row['programa']; ?>">
         </div>
 
-        <div class="input-group mb-3">
+        <div class="inputhola">
             <label for="lider">Líder:</label>
             <input id="lider" name="lider" type="text" value="<?php echo $row['lider']; ?>">
         </div>
 
         <button type="submit" name="guardar">Guardar</button>
-        <a class="A" href="index.php"><button>Volver a la página de inicio</button></a>
-
     </form>
+    <a class="A" href="index.php"><button>Volver a la página de inicio</button></a>
+    </div>
 </body>
 </html>
