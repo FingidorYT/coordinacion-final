d5972b6385106ebcd8bd6d70875fa4b2e2eb88d2
DanjVega001 <daniel.vega03@hotmail.com>
2023-10-19 07:45:34 -0500

    Funcionalidad de usuarios 1

--- comun/header.php
+++ comun/header.php
@@ -6,4 +6,5 @@ $env = parse_ini_file(dirname(__DIR__, 1).'/'.'.env');
         <img class="mintrabajo" src="/<?php echo $env['RUTA_PROYECTO'];?>/img/mintrabajo-logo.png">
         <img class="logosena" src="/<?php echo $env['RUTA_PROYECTO'];?>/img/logosena.png">
 </div>
-<a href="logout.php" class="salir_2"><button class="opcion salir" id="logout" >Logout</button></a>
+<!--Cambiar la ruta del logout php segun donde se eencuentra actualmente-->
+<a href="../logout.php" class="salir_2"><button class="opcion salir" id="logout" >Logout</button></a>
